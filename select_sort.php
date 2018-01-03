5e34a3d7383dc9720c9c910f4ed9907899a17524
subdued <subdued@subdueddeMacBook-Pro.local>
2018-01-03 11:30:35 +0800

    select_sort

---
+++ select_sort.php
@@ -0,0 +1,49 @@
+<?php
+	
+
+	$arr = [1,23,4,5,71,26,54,79,200,16];
+	/* bubble
+		function bubble($arr){
+
+			if(!is_array($arr)) return false;
+
+			for($i = 0;$i<count($arr);$i++){
+
+				for($j = 0;$j<count($arr)-$i-1;$j++){
+
+					if($arr[$j] < $arr[$j+1]){
+						$tmp = $arr[$j];
+						$arr[$j] = $arr[$j+1];
+						$arr[$j+1] = $tmp;
+					}
+				}
+			}
+
+			return $arr;
+		}
+
+		$new_arr = bubble($arr);
+	*/
+	// select_sort
+	function select_sort($arr){
+		if(!is_array($arr)) return false;
+
+		for($i = 0; $i<count($arr); $i++){
+			$p = $i; //假设p为最大
+			for($j = $i+1;$j<count($arr);$j++){
+				if($arr[$j]<$arr[$p]){
+					$p = $j;
+				}
+			}
+			if($p!=$i){
+				$tmp     = $arr[$i];
+				$arr[$i] = $arr[$p];
+				$arr[$p] = $tmp;  
+			}
+		}
+		return $arr;
+
+	}	
+	$new_arr = select_sort($arr);	
+	print_r($new_arr);
+
